3140d5d9ab60e523585bd42c4b9c0b70cabc3e58
Steve Kelly <kd2cca@gmail.com>
2014-08-07 01:39:14 -0400

    add generation of boundingbox types

--- src/BoundingBoxes.jl
+++ src/BoundingBoxes.jl
@@ -1,5 +1,16 @@
 module BoundingBoxes
 
-# package code goes here
+export @boundingbox
+
+macro boundingbox(ex)
+    # construct Bounding Box type
+    types = [Expr(:(::),symbol(axe*"_max"),:T) for axe in ex.args[2:end]]
+    append!(types,[Expr(:(::),symbol(axe*"_min"),:T) for axe in ex.args[2:end]])
+    curly = Expr(:curly, ex.args[1], Expr(:(<:), :T, :Number))
+    bound = Expr(:type, true, curly, Expr(:block, types...))
+    quote
+        $(esc(bound)) # create bounding box type
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
 using BoundingBoxes
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Initialize Bounding box
+@boundingbox Bounds2F, "x", "y"
+bb2 = Bounds2F{Float64}(1,2,3,4)
+
+@test bb2.x_max == 1
+@test bb2.y_max == 2
+@test bb2.x_min == 3
+@test bb2.y_min == 4
+
+@boundingbox Bounds2I, "x", "y"
+bb2 = Bounds2I{Int64}(4,3,2,1)
+
+@test bb2.x_max == 4
+@test bb2.y_max == 3
+@test bb2.x_min == 2
+@test bb2.y_min == 1
