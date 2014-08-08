8118308ee893dedba98a92556a05c990671374e0
Steve Kelly <kd2cca@gmail.com>
2014-08-08 14:37:45 -0400

    shebang tests, and add equality tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,27 @@
+#! /usr/bin/env julia
+
 using BoundingBoxes
 using Base.Test
 
 # Initialize Bounding box
 @boundingbox Bounds2F, "x", "y"
-bb2 = Bounds2F{Float64}(1,2,3,4)
+bb2f = Bounds2F{Float64}(1,2,3,4)
 
-@test bb2.x_max == 1
-@test bb2.y_max == 2
-@test bb2.x_min == 3
-@test bb2.y_min == 4
+@test bb2f.x_max == 1
+@test bb2f.y_max == 2
+@test bb2f.x_min == 3
+@test bb2f.y_min == 4
 
 @boundingbox Bounds2I, "x", "y"
-bb2 = Bounds2I{Int64}(4,3,2,1)
+bb2i = Bounds2I{Int64}(4,3,2,1)
 
-@test bb2.x_max == 4
-@test bb2.y_max == 3
-@test bb2.x_min == 2
-@test bb2.y_min == 1
+@test bb2i.x_max == 4
+@test bb2i.y_max == 3
+@test bb2i.x_min == 2
+@test bb2i.y_min == 1
+
+# test equality
+bb2e = Bounds2I{Int64}(4,3,2,1)
+
+@test bb2i == bb2e
+
